07d0b17d75022ff4394b40a720484dba2263c527
Yihong Wang <yh.wang@ibm.com>
2020-03-04 22:14:48 -0800

    Add implementation
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # node-red-contrib-post-object-detection
-Post-processing of object detection
+
+This is a custom Node-RED node that handles the prediction results of an Object
+Detection model.
+
+## Installation
+
+### Prerequisite
+For Tensorflow.js on Node.js
+([@tensorflow/tfjs-node](https://www.npmjs.com/package/@tensorflow/tfjs-node)
+or
+[@tensorflow/tfjs-node-gpu](https://www.npmjs.com/package/@tensorflow/tfjs-node-gpu)),
+it depends on Tensorflow shared libraries. Putting Tensorflow.js as the
+dependency of custom Node-RED node may run into a situation that multiple
+custom nodes install multiple `tfjs-node` module as their dependencies. While
+loading multiple Tensorflow shared libraries in the same process, the process
+would abord by hitting protobuf assertion.
+
+Therefore, this module put `@tensorflow/tfjs-node` as peer dependency. You need
+to install it with the Node-RED manully.
+
+Install `@tensorflow/tfjs-node`:
+```
+npm install @tensorflow/tfjs-node
+```
+
+### Install this module:
+Once you install the peer dependency, you can install this module:
+```
+npm install node-red-contrib-post-object-detection
+```
+
+## Usage
+
+The input for this node shall be an array of `tf.Tensorf` and its length is 2.
+First tensor is the detected objects with
+`[1, number of box detectors, number of classes]` shape. 1 is the batch size.
+The second tensor is the bounding boxes with
+`[1, number of box detectors, 1, 4]` shape. 4 is the four coordinates of the
+box. It also needs classes information. Please use the `Class URL` to assign
+a JSON file contains the `id` and `className` for each class. For example:
+```json
+{
+    "0": "person",
+    "1": "cup",
+    ...
+    ...
+}
+```
+Then it calculates the object detection results and returns the detected
+objects as an `Object[]`. Each object contains `bbox`, `className` and
+`score` properties.
+- bbox: the coordicates of the box, width and height: `[x1, y1, w, h]`.
+  They are float number between 0.0 and 1.0.
+- className: the name of the class
+- score: the confident value between 0.0 and 1.0.
---
+++ src/index.ts
@@ -0,0 +1,253 @@
+import * as tf from '@tensorflow/tfjs-node';
+import { request as requestHTTPS } from 'https';
+import { request as requestHTTP, IncomingMessage, ClientRequest } from 'http';
+
+interface DetectedObject {
+  bbox: [number, number, number, number];  // [x, y, width, height]
+  className: string;
+  score: number;
+}
+
+/**
+ * Represent Node-Red's runtime
+ */
+type NodeRed = {
+  nodes: NodeRedNodes;
+};
+
+type NodeRedWire = {
+  [index: number]: string;
+};
+
+type NodeRedWires = {
+  [index: number]: NodeRedWire;
+};
+
+/**
+ * Represent Node-Red's configuration for a custom node
+ * For this case, it's the configuration for tf-model node
+ */
+type NodeRedProperties = {
+  id: string;
+  type: string;
+  name: string;
+  classesURL: string;
+  wires: NodeRedWires;
+};
+
+/**
+ * Represent Node-Red's nodes
+ */
+type NodeRedNodes = {
+  // tslint:disable-next-line:no-any
+  createNode(node: any, props: NodeRedProperties): void;
+  // tslint:disable-next-line:no-any
+  registerType(type: string, ctor: any): void;
+};
+
+/**
+ * Represent Node-Red's message that passes to a node
+ */
+type NodeRedReceivedMessage = {
+  payload: tf.Tensor[];
+};
+
+type NodeRedSendMessage = {
+  payload: DetectedObject[];
+};
+
+type ImageClasses = {
+  [index: string]: string;
+};
+
+type StatusOption = {
+  fill?: 'red' | 'green' | 'yellow' | 'blue' | 'grey';
+  shape?: 'ring' | 'dot';
+  text?: string;
+};
+
+/**
+ * Wrapper function for http and https
+ * @param url
+ * @param callback
+ */
+function request(url: URL, callback?: (res: IncomingMessage) => void)
+    : ClientRequest {
+
+    const reqOpt = {
+    hostname: url.hostname,
+    port: url.port,
+    path: url.pathname,
+    method: 'GET'
+  };
+
+  if (url.protocol === 'http:') {
+    return requestHTTP(reqOpt, callback);
+  } else if (url.protocol === 'https:') {
+    return requestHTTPS(reqOpt, callback);
+  }
+  return undefined;
+}
+
+/**
+ * Fetch Classes JSON doc
+ * @param url file path, http url or https url. Need to contains
+ *            the proper scheme, i.e. file:// or http://
+ */
+function fetchClasses(urlStr: string): Promise<ImageClasses> {
+  let url: URL;
+  try {
+    url = new URL(urlStr);
+  } catch (e) {
+    return Promise.reject('Invalid URL');
+  }
+
+  if (url.protocol === 'file:') {
+    return Promise.resolve(require(url.pathname));
+  }
+
+  return new Promise<ImageClasses>((resolve, reject) => {
+    const chunks: string[] = [];
+    const req = request(url, (res: IncomingMessage) => {
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => chunks.push(chunk));
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          resolve(JSON.parse(chunks.join('')));
+        } else {
+          reject(`can not fetch classes file: ${res.statusCode}, ${res.statusMessage}`);
+        }
+      });
+    });
+
+    if (req === undefined) {
+      reject('unsupported protocol');
+    } else {
+      req.on('error', (e) => {
+        reject(e.message);
+      });
+      req.end();
+    }
+  });
+}
+
+// Module for a Node-Red custom node
+export = function postObjectDetection(RED: NodeRed) {
+
+  class PostObjectDetection {
+    // tslint:disable-next-line:no-any
+    on: (event: string, fn: (msg: any) => void) => void;
+    send: (msg: NodeRedSendMessage) => void;
+    status: (option: StatusOption) => void;
+    log: (msg: string) => void;
+    getClassName: (idx: number) => string;
+
+    classesURL: string;
+    classes: ImageClasses;
+    maxNumBoxes = 20;
+
+    constructor(config: NodeRedProperties) {
+      this.classesURL = config.classesURL.trim();
+
+      RED.nodes.createNode(this, config);
+      this.on('input', (msg: NodeRedReceivedMessage) => {
+        this.handleRequest(msg.payload);
+      });
+
+      this.on('close', (done: () => void) => {
+        this.handleClose(done);
+      });
+
+      // By default, use id as the class name
+      this.getClassName = (idx: number) => {
+        return `${idx}`;
+      };
+
+      if (this.classesURL.length > 0) {
+        fetchClasses(this.classesURL)
+            .then((rev) => this.classes = rev)
+            .then(() => {
+              this.status({});
+              this.getClassName = (idx: number) => {
+                return this.classes[idx];
+              };
+            })
+            .catch((error: string) => {
+              this.log('can not fetch classes file, using id as class name instead');
+              this.status({fill:'red' ,shape:'dot', text:error});
+            });
+      }
+    }
+
+    // Handle a single request
+    // The following implementation is from:
+    // https://github.com/tensorflow/tfjs-models/blob/master/coco-ssd/src/index.ts
+    handleRequest(inputs: tf.Tensor[]) {
+      const scores = inputs[0].dataSync() as Float32Array;
+      const boxes = inputs[1].dataSync() as Float32Array;
+      tf.dispose(inputs);
+      const [maxScores, classes] =
+        this.calculateMaxScores(scores, inputs[0].shape[1], inputs[0].shape[2]);
+      const indexTensor = tf.tidy(() => {
+        const boxes2 =
+            tf.tensor2d(boxes, [inputs[1].shape[1], inputs[1].shape[3]]);
+        return tf.image.nonMaxSuppression(
+            boxes2, maxScores, this.maxNumBoxes, 0.5, 0.5);
+      });
+      const indexes = indexTensor.dataSync() as Float32Array;
+      indexTensor.dispose();
+
+      const obj = this.buildDetectedObjects(
+          boxes, maxScores, indexes, classes);
+      this.send({payload: obj});
+    }
+
+    handleClose(done: () => void) {
+      // node level clean up
+      done();
+    }
+
+    buildDetectedObjects(boxes: Float32Array, scores: number[],
+        indexes: Float32Array, classes: number[]): DetectedObject[] {
+      const count = indexes.length;
+      const objects: DetectedObject[] = [];
+      for (let i = 0; i < count; i++) {
+        const bbox = [
+          boxes[indexes[i] * 4],
+          boxes[indexes[i] * 4 + 1],
+          boxes[indexes[i] * 4 + 2],
+          boxes[indexes[i] * 4 + 3]
+        ];
+
+        objects.push({
+          bbox: [bbox[1], bbox[0], bbox[3] - bbox[1], bbox[2] - bbox[0]],
+          className: this.getClassName(classes[indexes[i]]),
+          score: scores[indexes[i]]
+        });
+      }
+      return objects;
+    }
+
+    calculateMaxScores(
+        scores: Float32Array, numBoxes: number,
+        numClasses: number): [number[], number[]] {
+      const maxes = [];
+      const classes = [];
+      for (let i = 0; i < numBoxes; i++) {
+        let max = Number.MIN_VALUE;
+        let index = -1;
+        for (let j = 0; j < numClasses; j++) {
+          if (scores[i * numClasses + j] > max) {
+            max = scores[i * numClasses + j];
+            index = j;
+          }
+        }
+        maxes[i] = max;
+        classes[i] = index;
+      }
+      return [maxes, classes];
+    }
+  }
+
+  RED.nodes.registerType('post-object-detection', PostObjectDetection);
+};
